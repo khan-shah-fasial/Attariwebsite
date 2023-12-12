7be66b1f366628772e7c84d8dbff9c08c44353b0
Faisal <khanfasial.makent@gmail.com>
2023-12-12 17:43:33 +0530

    first commit

---
+++ resources/views/frontend/component/get_in_touch.blade.php
@@ -0,0 +1,30 @@
+<!------================= Get in touch ============= ------------------------->
+
+
+
+<section class="">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="get_in_touch">
+                <div class="row">
+                  <div class="col-md-6">
+                    <p class="color_heading">GET IN TOUCH</p>
+                    <h2 class="heading">
+                      Work With Us. Let's Make Great Together.
+                    </h2>
+                    <p>Our Best Skilled Attorneys, Trust Score 5.0 <img src="assets/frontend/images/5-star.png" alt=""> </p>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="d-flex justify-content-md-end justify-content-center mt-md-4 mt-2" >
+                    <a class="number"> <img src="assets/frontend/images/call_white.png" alt="">+91-7428899959</a>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+<!------================= Get in touch ============= ------------------------->
