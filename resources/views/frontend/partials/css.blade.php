570ec97faa60c3c68d2210ec809a0817ebbcab99
junaid53 <ansarijunaid53@gmial.com>
2024-05-07 12:48:48 +0530

    css blade changes

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,18 +1,14 @@
 
 <link rel="shortcut icon" href="{{ asset('/assets/frontend/images/favicon.webp') }}">
 
-<link rel="stylesheet" href="/assets/frontend/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/assets/frontend/css/style.css" /> 
-<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="/assets/frontend/css/responsive.css" />
-<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="/assets/frontend/css/bootstrap.min.css" />
-<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="/assets/frontend/css/owl.carousel.min.css" />
-<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="/assets/frontend/css/owl.theme.default.min.css" />
-<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="/assets/frontend/css/toastr.min.css" />
-<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/assets/frontend/css/responsive.css" />
+<link rel="stylesheet" href="/assets/frontend/css/bootstrap.min.css" />
+<link rel="stylesheet" href="/assets/frontend/css/owl.carousel.min.css" />
+<link rel="stylesheet" href="/assets/frontend/css/owl.theme.default.min.css" />
+<link rel="stylesheet" href="/assets/frontend/css/toastr.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <link rel="stylesheet" href="/assets/frontend/css/owl.theme.default.css" />
 <link rel="stylesheet" href="/assets/frontend/css/fancybox.min.css"/>
 <link rel="stylesheet" href="/assets/frontend/css/robotfonts.css" >
-
-
-
